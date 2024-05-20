16718cb10e1a0f1bda33bba852f43e042708542a
Codapop <george.dikun91@gmail.com>
2024-05-21 00:56:04 +0800

    Fix utilities.js

--- utilities.js
+++ utilities.js
@@ -6,13 +6,6 @@
 
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-function cleanStupidDate1900Issue(currentDateTime) {
-    let year = currentDateTime.getFullYear();
-    let dateToFix = new Date(currentDateTime);
-    dateToFix.setFullYear(year);
-    return dateToFix;
-}
-
 function getMidnightInUTC(dateToFind, utcMidnight) {
     let midnightInChina = new Date(dateToFind);
     midnightInChina.setUTCDate(dateToFind.getDate()-1);
@@ -23,6 +16,7 @@ function getMidnightInUTC(dateToFind, utcMidnight) {
     return midnightInChina;
 }
 
+// Takes two dates and returns the difference between them in days
 function differenceInDays(date1, date2) {
     return (date1 - date2)/1000/60/60/24;
 }
