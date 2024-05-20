ff4710bccbc246ea5d5e4efcb066f8e811715b87
Tommaso Ornato <tommaso.ornato@dialogueexplorebond.it>
2024-05-20 10:41:57 +0200

    Extracting file paths from HTML

--- actions/release.py
+++ actions/release.py
@@ -1,8 +1,37 @@
+from bs4 import BeautifulSoup
 import minify_html
 
-with open('../index.html', 'r', encoding='utf-8') as in_file:
-    html_string = in_file.read()
-    minified = minify_html.minify(html_string, minify_js=True, minify_css=True, remove_processing_instructions=True)
+def main():
+    with open('../index.html', 'r', encoding='utf-8') as in_file:
+        soup = BeautifulSoup(in_file, 'lxml')
 
-    with open('minified.html', 'w') as out_file:
-        out_file.write(minified)
+        scripts = soup.find_all('script')
+        styles = soup.find('link', rel='stylesheet')
+        favicon = soup.find('link', rel='icon')
+
+        scripts = parse_scripts(scripts)
+        styles = parse_styles(styles)
+        favicon_link = parse_favicon(favicon)
+
+        print(scripts)
+        print(styles)
+        print(favicon_link)
+
+
+def parse_scripts(scripts):
+    scripts = list(map(lambda el: el.get('src'), scripts))
+
+    return scripts
+
+def parse_styles(styles):
+    styles = styles['href']
+
+    return styles
+
+def parse_favicon(favicon):
+    favicon_link = favicon['href']
+
+    return favicon_link
+
+if __name__ == "__main__":
+    main()
