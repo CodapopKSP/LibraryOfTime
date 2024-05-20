a268757c7fe7dbf3b9ebfd92073cd94971876023
Tommaso Ornato <tommaso.ornato@dialogueexplorebond.it>
2024-05-20 11:16:07 +0200

    Parsing favicon

--- actions/release.py
+++ actions/release.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import minify_html
+import base64
 
 def main():
     with open('../index.html', 'r', encoding='utf-8') as in_file:
@@ -21,7 +22,7 @@ def main():
 
         # print(scripts)
         # print(styles)
-        print(favicon_link)
+        # print(favicon_link)
 
 
 # Returns a string containing the content of all script tags
@@ -50,8 +51,16 @@ def parse_styles(styles):
 # Returns a string containing the Base64 conversion of a favicon image
 def parse_favicon(favicon):
     favicon_link = favicon['href']
+    path = f'../{favicon_link}'    # Adjust path for static directory
 
-    return favicon_link
+    # Open the image file in binary mode
+    with open(path, "rb") as image_file:
+        image_data = image_file.read()
+
+        base64_encoded_image = base64.b64encode(image_data)
+        base64_string = base64_encoded_image.decode('utf-8')    # Convert base64 bytes to string
+
+        return base64_string
 
 if __name__ == "__main__":
     main()
