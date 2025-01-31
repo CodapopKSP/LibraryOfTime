45223fcd527af9916714f9f8d361b94fdb3b1926
CodapopKSP <george.dikun91@gmail.com>
2025-02-01 01:01:16 +0800

    Test the tests

--- script.js
+++ script.js
@@ -277,38 +277,39 @@ function changeDateTime(newDateString = '', timezonePassthrough = '') {
         }, 1);
     }
 }
+if (typeof document !== "undefined") {
+    document.addEventListener('DOMContentLoaded', function () {
+        // Masonry Tiling library
+        var grid = document.querySelector('.node-wrapper');
+        var msnry = new Masonry(grid, {
+            itemSelector: '.container',
+            columnWidth: '.container',
+            percentPosition: true,
+        });
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Masonry Tiling library
-    var grid = document.querySelector('.node-wrapper');
-    var msnry = new Masonry(grid, {
-        itemSelector: '.container',
-        columnWidth: '.container',
-        percentPosition: true,
+        // User's timezone dropdown selection
+        const timezoneSelect = document.getElementById('timezone');
+        const timezones = [
+            'UTC-12:00', 'UTC-11:00', 'UTC-10:00', 'UTC-09:30', 'UTC-09:00',
+            'UTC-08:00', 'UTC-07:00', 'UTC-06:00', 'UTC-05:00', 'UTC-04:00',
+            'UTC-03:30', 'UTC-03:00', 'UTC-02:00', 'UTC-01:00', 'UTC+00:00',
+            'UTC+01:00', 'UTC+02:00', 'UTC+03:00', 'UTC+03:30', 'UTC+04:00',
+            'UTC+04:30', 'UTC+05:00', 'UTC+05:30', 'UTC+05:45', 'UTC+06:00',
+            'UTC+06:30', 'UTC+07:00', 'UTC+08:00', 'UTC+08:45', 'UTC+09:00',
+            'UTC+09:30', 'UTC+10:00', 'UTC+10:30', 'UTC+11:00', 'UTC+12:00',
+            'UTC+12:45', 'UTC+13:00', 'UTC+14:00'
+        ];
+        timezones.forEach(timezone => {
+            const option = document.createElement('option');
+            option.value = timezone;
+            option.text = timezone;
+            if (timezone === getFormattedTimezoneOffset()) {
+                option.selected = true;
+            }
+            timezoneSelect.appendChild(option);
+        });
     });
-
-    // User's timezone dropdown selection
-    const timezoneSelect = document.getElementById('timezone');
-    const timezones = [
-        'UTC-12:00', 'UTC-11:00', 'UTC-10:00', 'UTC-09:30', 'UTC-09:00',
-        'UTC-08:00', 'UTC-07:00', 'UTC-06:00', 'UTC-05:00', 'UTC-04:00',
-        'UTC-03:30', 'UTC-03:00', 'UTC-02:00', 'UTC-01:00', 'UTC+00:00',
-        'UTC+01:00', 'UTC+02:00', 'UTC+03:00', 'UTC+03:30', 'UTC+04:00',
-        'UTC+04:30', 'UTC+05:00', 'UTC+05:30', 'UTC+05:45', 'UTC+06:00',
-        'UTC+06:30', 'UTC+07:00', 'UTC+08:00', 'UTC+08:45', 'UTC+09:00',
-        'UTC+09:30', 'UTC+10:00', 'UTC+10:30', 'UTC+11:00', 'UTC+12:00',
-        'UTC+12:45', 'UTC+13:00', 'UTC+14:00'
-    ];
-    timezones.forEach(timezone => {
-        const option = document.createElement('option');
-        option.value = timezone;
-        option.text = timezone;
-        if (timezone === getFormattedTimezoneOffset()) {
-            option.selected = true;
-        }
-        timezoneSelect.appendChild(option);
-    });
-});
+}
 
 // Get local timezone for dropdown menu default option
 function getFormattedTimezoneOffset() {
