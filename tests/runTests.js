73ff5670b89db1d384bb262b676ac19c56f29219
CodapopKSP <george.dikun91@gmail.com>
2025-02-04 23:56:10 +0800

    test the tests

--- tests/runTests.js
+++ tests/runTests.js
@@ -1,6 +1,38 @@
-// Run all tests
-testParseDate();
-testTimezoneFormatter();
-testCalendarType();
-testGregorianCalendar();
-testJulianCalendar();
+(function(global) {
+    // Declare variables to hold our test functions.
+    let testParseDate,
+        testTimezoneFormatter,
+        testCalendarType,
+        testGregorianCalendar,
+        testJulianCalendar;
+        
+    // If running in Node, use require() to load the exports.
+    if (typeof require !== 'undefined' && typeof module !== 'undefined' && module.exports) {
+      // Adjust the relative path as needed.
+      ({ 
+        testParseDate, 
+        testTimezoneFormatter, 
+        testCalendarType, 
+        testGregorianCalendar, 
+        testJulianCalendar 
+      } = require('./calendarTests.js'));
+    } else {
+      // In the browser, assume the functions are attached to the global object (window).
+      ({ 
+        testParseDate, 
+        testTimezoneFormatter, 
+        testCalendarType, 
+        testGregorianCalendar, 
+        testJulianCalendar 
+      } = global);
+    }
+    
+    // Run all tests.
+    testParseDate();
+    testTimezoneFormatter();
+    testCalendarType();
+    testGregorianCalendar();
+    testJulianCalendar();
+    
+  })(typeof window !== 'undefined' ? window : global);
+  
