15ab6a1f6567cdd36845f253e5225f173995865f
CodapopKSP <george.dikun91@gmail.com>
2025-06-14 15:06:32 +0800

    Reorganize tests.

--- tests/solarCalendarTests.js
+++ tests/solarCalendarTests.js
@@ -1,92 +1,7 @@
 
-import {convertUTCOffsetToMinutes, parseInputDate, adjustCalendarType, setCalendarType} from '../userInterface.js';
+import {convertUTCOffsetToMinutes, parseInputDate} from '../userInterface.js';
 import * as solarCalendars from '../Calendars/solarCalendars.js';
 import * as astronomicalData from '../Other/astronomicalData.js';
-
-
-function testTimezoneFormatter() {
-    let testCount = 0;
-    let passedTestCount = 0;
-
-    function testTimezoneFormatter_test(timezoneTest, minutesTest) {
-        testCount += 1;
-        let minutes = convertUTCOffsetToMinutes(timezoneTest);
-        if (minutes === minutesTest) {
-            passedTestCount += 1;
-        } else {
-            console.error('Timezone Offset: Test ' + testCount + ' failed.');
-        }
-    }
-
-    // Tests - Timezone, Output Minutes
-    testTimezoneFormatter_test('UTC+08:00', 480);
-    testTimezoneFormatter_test('UTC+00:00', 0);
-    testTimezoneFormatter_test('UTC-12:00', -720);
-
-    // Cumulative Test Check
-    if (testCount === passedTestCount) {
-        console.log('Timezone Offset: All Tests Passed');
-    }
-}
-
-function testParseDate() {
-    let testCount = 0;
-    let passedTestCount = 0;
-
-    function testParseDate_test(testedDate, testedTimezone, parsedDateTest) {
-        testCount += 1;
-        let parsedDate = parseInputDate(testedDate, testedTimezone);
-        parsedDate = parsedDate.toUTCString();
-        if (parsedDate === parsedDateTest) {
-            passedTestCount += 1;
-        } else {
-            console.error('Parse Date: Test ' + testCount + ' failed.');
-            console.error(parsedDate);
-        }
-    }
-
-    // Tests - Input Date, Timezone, Output Date
-    testParseDate_test('1950-1-1, 00:00:00', 'UTC+00:00', 'Sun, 01 Jan 1950 00:00:00 GMT');
-    testParseDate_test('2000-12-31, 00:00:00', 'UTC+08:00', 'Sat, 30 Dec 2000 16:00:00 GMT');
-    testParseDate_test('2016-2-29, 00:00:00', 'UTC+12:00', 'Sun, 28 Feb 2016 12:00:00 GMT');
-    testParseDate_test('0001-1-2, 00:00:00', 'UTC-10:00', 'Tue, 02 Jan 0001 10:00:00 GMT');
-
-    // Cumulative Test Check
-    if (testCount === passedTestCount) {
-        console.log('Parse Date: All Tests Passed');
-    }
-}
-
-function testCalendarType() {
-    let testCount = 0;
-    let passedTestCount = 0;
-
-    function testCalendarType_test(testedDate_, testedTimezone, parsedDateTest) {
-        testCount += 1;
-        let testedDate = parseInputDate(testedDate_, testedTimezone);
-        let parsedDate = adjustCalendarType(testedDate);
-        parsedDate = parsedDate.toUTCString();
-        if (parsedDate === parsedDateTest) {
-            passedTestCount += 1;
-        } else {
-            console.error('Calendar Type: Test ' + testCount + ' failed.');
-            console.error(parsedDate);
-        }
-    }
-
-    // Tests - Input Date, Calendar Type, Output Date
-    testCalendarType_test("2024-1-1, 00:00:00", "UTC+00:00", 'Mon, 01 Jan 2024 00:00:00 GMT');
-    setCalendarType('julian-liturgical');
-    testCalendarType_test("2000-1-1, 23:00:00", "UTC+00:00", 'Fri, 14 Jan 2000 23:00:00 GMT');
-    setCalendarType('astronomical');
-    testCalendarType_test("1000-1-1, 00:00:00", "UTC+00:00", 'Mon, 06 Jan 1000 00:00:00 GMT');
-    setCalendarType('gregorian-proleptic');
-
-    // Cumulative Test Check
-    if (testCount === passedTestCount) {
-        console.log('Calendar Type: All Tests Passed');
-    }
-}
 
 function testGregorianCalendar() {
     let testCount = 0;
@@ -398,9 +313,6 @@ function testHaabCalendar() {
 }
 
 // Run all tests.
-testParseDate();
-testTimezoneFormatter();
-testCalendarType();
 testGregorianCalendar();
 testJulianCalendar();
 testAstronomicalCalendar();
