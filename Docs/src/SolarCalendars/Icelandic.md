f6e56d8cb560f593cd53cf5a2793d91f0781955a
CodapopKSP <george.dikun91@gmail.com>
2025-12-16 00:59:54 +0800

    Fix typos and general phrasing.

--- Docs/src/SolarCalendars/Icelandic.md
+++ Docs/src/SolarCalendars/Icelandic.md
@@ -6,15 +6,15 @@
 
 #### Overview
 
-The Icelandic calendar is the traditional calendar of Iceland. It features two seasons, Summer and Winter, called 'misseri' meaning 'semester', each with 26-28 weeks. There is no epoch, and years do not necessarily begin with either messeri.
+The Icelandic calendar is the traditional calendar of Iceland. It features two seasons, Summer and Winter, called 'misseri' meaning 'semester', each with 26-28 weeks. There is no epoch, and years do not necessarily begin with either misseri.
 
-Although the messeri and weeks are the primary units, the messeri are further broken down into 6 months each, all of which consist of 30 days for a total of 360 days. Another four days are added between the 3rd and 4th months of Summer (midsummer) called 'Aukanætur' and is treated as a holiday. This brings the year to exactly 52 weeks or 364 days.
+Although the misseri and weeks are the primary units, the misseri are further broken down into 6 months each, all of which consist of 30 days for a total of 360 days. Another four days are added between the 3rd and 4th months of Summer (midsummer) called 'Aukanætur' and is treated as a holiday. This brings the year to exactly 52 weeks or 364 days.
 
 In order to keep the calendar aligned with the solar year, an intercalary week is added every 7 years, called 'Sumarauki' and occurring immediately after Aukanætur. Modern intercalary rules ensure the start of Summer always happens on the Thursday that occurs between 19-25th April of the Gregorian calendar (and previously 9-15th of the Julian calendar.)
 
 #### Info
 
-Because of the 4 days of Aukanætur, Summer is slightly longer than Winter, and neither is divisible cleanly by the 7-day week. The beginning of Winter is not on the same day of the week as the beginning of Summer. Instead, the beginning of the week shifts depending on the current misseri, resulting in the final week of each messeri being incomplete.
+Because of the 4 days of Aukanætur, Summer is slightly longer than Winter, and neither is divisible cleanly by the 7-day week. The beginning of Winter is not on the same day of the week as the beginning of Summer. Instead, the beginning of the week shifts depending on the current misseri, resulting in the final week of each misseri being incomplete.
 
 | Month | Days | Season |
 |-------|------|--------|
@@ -35,7 +35,7 @@ Because of the 4 days of Aukanætur, Summer is slightly longer than Winter, and 
 
 #### Accuracy
 
-This calendar has a long history with several slight changes, especially regarding modern/old names and specific starting and ending times of messeri and weeks. I am reasonably sure it is accurate to official sources, but the information is somewhat fragmented.
+This calendar has a long history with several slight changes, especially regarding modern/old names and specific starting and ending times of misseri and weeks. I am reasonably sure it is accurate to official sources, but the information is somewhat fragmented.
 
 #### Source
 
--- nodeData.js
+++ nodeData.js
@@ -856,11 +856,24 @@ const solarCalendarsData = [
         type: 'Solar Calendar',
         epoch: 'Unknown',
         confidence: 'High',
-        overview: `The Icelandic calendar is the traditional calendar of Iceland. It features two seasons, Summer and Winter, called 'misseri' meaning 'semester', each with 26-28 weeks. There is no epoch, and years do not necessarily begin with either messeri.\n\nAlthough the messeri and weeks are the primary units, the messeri are further broken down into 6 months each, all of which consist of 30 days for a total of 360 days. Another four days are added between the 3rd and 4th months of Summer (midsummer) called 'Aukanætur' and is treated as a holiday. This brings the year to exactly 52 weeks or 364 days.\n\nIn order to keep the calendar aligned with the solar year, an intercalary week is added every 7 years, called 'Sumarauki' and occurring immediately after Aukanætur. Modern intercalary rules ensure the start of Summer always happens on the Thursday that occurs between 19-25th April of the Gregorian calendar (and previously 9-15th of the Julian calendar.)`,
-        info: `Because of the 4 days of Aukanætur, Summer is slightly longer than Winter, and neither is divisible cleanly by the 7-day week. The beginning of Winter is not on the same day of the week as the beginning of Summer. Instead, the beginning of the week shifts depending on the current misseri, resulting in the final week of each messeri being incomplete.\n\n<table class=table-very-very-long><tr><th colspan="3">Icelandic Months</th></tr><tr><th>Month</th><th>Days</th><th>Season</th></tr><tr><td>Harpa</td><td>30</td><td rowspan="8">Summer</td></tr><tr><td>Skerpla</td><td>30</td></tr><tr><td>Sólmánuðr</td><td>30</td></tr><tr><td>Aukanætur (Midsummer)</td><td>4</td></tr><tr><td>Sumarauki (Leap Year)</td><td>7</td></tr><tr><td>Heyannir</td><td>30</td></tr><tr><td>Tvímánuðr</td><td>30</td></tr><tr><td>Haustmánuðr</td><td>30</td></tr><tr><td>Gormánuðr</td><td>30</td><td rowspan="6">Winter</td></tr><tr><td>Ýlir</td><td>30</td></tr><tr><td>Mörsugr</td><td>30</td></tr><tr><td>Þorri</td><td>30</td></tr><tr><td>Góa</td><td>30</td></tr><tr><td>Einmánuðr</td><td>30</td></tr></table>`,
-        accuracy: `This calendar has a long history with several slight changes, especially regarding modern/old names and specific starting and ending times of messeri and weeks. I am reasonably sure it is accurate to official sources, but the information is somewhat fragmented.`,
+        overview: `The Icelandic calendar is the traditional calendar of Iceland. It features two seasons, Summer and Winter, called 'misseri' meaning 'semester', each with 26-28 weeks. There is no epoch, and years do not necessarily begin with either misseri.\n\nAlthough the misseri and weeks are the primary units, the misseri are further broken down into 6 months each, all of which consist of 30 days for a total of 360 days. Another four days are added between the 3rd and 4th months of Summer (midsummer) called 'Aukanætur' and is treated as a holiday. This brings the year to exactly 52 weeks or 364 days.\n\nIn order to keep the calendar aligned with the solar year, an intercalary week is added every 7 years, called 'Sumarauki' and occurring immediately after Aukanætur. Modern intercalary rules ensure the start of Summer always happens on the Thursday that occurs between 19-25th April of the Gregorian calendar (and previously 9-15th of the Julian calendar.)`,
+        info: `Because of the 4 days of Aukanætur, Summer is slightly longer than Winter, and neither is divisible cleanly by the 7-day week. The beginning of Winter is not on the same day of the week as the beginning of Summer. Instead, the beginning of the week shifts depending on the current misseri, resulting in the final week of each misseri being incomplete.\n\n<table class=table-very-very-long><tr><th colspan="3">Icelandic Months</th></tr><tr><th>Month</th><th>Days</th><th>Season</th></tr><tr><td>Harpa</td><td>30</td><td rowspan="8">Summer</td></tr><tr><td>Skerpla</td><td>30</td></tr><tr><td>Sólmánuðr</td><td>30</td></tr><tr><td>Aukanætur (Midsummer)</td><td>4</td></tr><tr><td>Sumarauki (Leap Year)</td><td>7</td></tr><tr><td>Heyannir</td><td>30</td></tr><tr><td>Tvímánuðr</td><td>30</td></tr><tr><td>Haustmánuðr</td><td>30</td></tr><tr><td>Gormánuðr</td><td>30</td><td rowspan="6">Winter</td></tr><tr><td>Ýlir</td><td>30</td></tr><tr><td>Mörsugr</td><td>30</td></tr><tr><td>Þorri</td><td>30</td></tr><tr><td>Góa</td><td>30</td></tr><tr><td>Einmánuðr</td><td>30</td></tr></table>`,
+        accuracy: `This calendar has a long history with several slight changes, especially regarding modern/old names and specific starting and ending times of misseri and weeks. I am reasonably sure it is accurate to official sources, but the information is somewhat fragmented.`,
         source: `All of the information for this calendar came from <a href="https://www2.math.uu.se/~svantejs/papers/calendars/iceland.pdf">this paper</a>.`
     },
+
+    /*
+    {
+        name: 'Śaka Samvat',
+        id: 'saka-samvat',
+        type: 'Solar Calendar',
+        epoch: '',
+        confidence: '',
+        overview: ``,
+        info: ``,
+        accuracy: ``,
+        source: ``
+    }*/
 ]
 
 const lunisolarCalendarsData = [
