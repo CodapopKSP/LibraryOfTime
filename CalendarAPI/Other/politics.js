1499c5b44575e2787343d388e741187bb20a53e5
CodapopKSP <george.dikun91@gmail.com>
2026-07-03 00:00:37 +0800

    Fix US presidential terms jan 20 bug

--- CalendarAPI/Other/politics.js
+++ CalendarAPI/Other/politics.js
@@ -6,18 +6,23 @@
 
 const PRESIDENTIAL_EPOCH_YEAR = 1789;
 const PRESIDENTIAL_TERM_LENGTH_YEARS = 4;
-const PRESIDENTIAL_DAYS_PER_YEAR = 365.25;
 const PRESIDENTIAL_TZ = 'UTC-04:00';
 
 function getCurrentPresidentialTerm(currentDateTime) {
     const januaryThisYear = createAdjustedDateTime({ currentDateTime, timezone: PRESIDENTIAL_TZ, month: 1, day: 20, hour: 'NOON' });
-    const january1789 = createAdjustedDateTime({ timezone: PRESIDENTIAL_TZ, year: PRESIDENTIAL_EPOCH_YEAR, month: 1, day: 20, hour: 'NOON' });
 
     let yearsSinceEpoch = currentDateTime.getFullYear() - PRESIDENTIAL_EPOCH_YEAR;
     if (currentDateTime < januaryThisYear) {
         yearsSinceEpoch -= 1;
     }
 
-    const currentPresidentialTerm = differenceInDays(currentDateTime, january1789) / PRESIDENTIAL_DAYS_PER_YEAR / PRESIDENTIAL_TERM_LENGTH_YEARS;
+    const termsSinceEpoch = Math.floor(yearsSinceEpoch / PRESIDENTIAL_TERM_LENGTH_YEARS);
+    const currentTermStartYear = PRESIDENTIAL_EPOCH_YEAR + termsSinceEpoch * PRESIDENTIAL_TERM_LENGTH_YEARS;
+    const nextTermStartYear = currentTermStartYear + PRESIDENTIAL_TERM_LENGTH_YEARS;
+
+    const currentTermStart = createAdjustedDateTime({ timezone: PRESIDENTIAL_TZ, year: currentTermStartYear, month: 1, day: 20, hour: 'NOON' });
+    const nextTermStart = createAdjustedDateTime({ timezone: PRESIDENTIAL_TZ, year: nextTermStartYear, month: 1, day: 20, hour: 'NOON' });
+
+    const currentPresidentialTerm = termsSinceEpoch + differenceInDays(currentDateTime, currentTermStart) / differenceInDays(nextTermStart, currentTermStart);
     return currentPresidentialTerm;
 }
